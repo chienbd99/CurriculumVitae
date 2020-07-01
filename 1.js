7b69401074c510941580e4bca10133a7d914e5b4
unknown <bdc23012000@gmail.com>
2020-07-01 23:34:46 +0700

    upload cv

---
+++ 1.js
@@ -0,0 +1,22 @@
+﻿document.addEventListener("DOMContentLoaded",function(){
+	var cacnut=document.querySelectorAll('.nut .fa'),
+		cacinfo=document.querySelectorAll('.if');
+		console.log(cacnut);
+		console.log(cacinfo);
+		for(var i=0;i<cacnut.length;i++){
+			cacnut[i].addEventListener('click',function(){
+				for (var i = 0; i < cacnut.length; i++) {
+					cacnut[i].classList.remove('activenut');
+				}
+				this.classList.add('activenut');
+				var nameif=this.getAttribute('data-name');
+				for (var i = 0; i < cacinfo.length; i++) {
+					cacinfo[i].classList.remove('active');
+				}
+				var ifhientai=document.querySelector('.'+nameif);
+				ifhientai.classList.add('active');
+			})
+		}
+
+},false)
+ 
